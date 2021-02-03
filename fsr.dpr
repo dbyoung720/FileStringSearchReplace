403f6ae1f2cefaba9b4dc248bc7100a046da8c82
dbyoung720 <dbyoung@sina.com>
2021-02-03 09:02:55 +0800

    update fsr.dpr

--- fsr.dpr
+++ fsr.dpr
@@ -12,6 +12,8 @@ uses
   System.Types,
   System.IOUtils,
   System.Classes,
+  Xml.XMLDoc,
+  Xml.XMLIntf,
   System.SysUtils;
 
 procedure ShowHelp;
@@ -19,12 +21,44 @@ begin
   Writeln('功能 : 在文件中进行字符串的搜索替换');
   Writeln('作者 : dbyoung@sina.com');
   Writeln('时间 : 2020-08-16');
-  Writeln('格式 : fsr [文件路径],[文件类型],[待替换字符串],[替换字符串],[是否包含子目录],[是否区分大小写],[文件保存编码格式]');
-  Writeln('示范 : fsr ''C:\Windows'',''*.txt'',''AAA'',''BBB'',1,0,''utf8''');
-  Writeln('注意 : 参数用符号,分割；中间不能有空格。前四个参数必须，后三个参数可省略');
+  Writeln('格式 : fsr [文件路径],[文件类型],[待替换字符串],[替换字符串],[是否包含子目录],[是否区分大小写],[文件保存编码格式],[VC 工程添加 MT 编译]');
+  Writeln('示范 : fsr ''C:\Windows'',''*.txt'',''AAA'',''BBB'',1,0,''utf8'', 1');
+  Writeln('注意 : 参数用符号,分割；中间不能有空格。前四个参数必须，后四个参数可省略');
 end;
 
-procedure SearchAndReplaceInFile(const arrFile: TStringDynArray; const bCase: Boolean; const strSearch, strReplace, strEncoding: string);
+{ 给 VC 工程添加 MT 编译 }
+procedure VCAddMT(const strFileName: string);
+var
+  xmlDocument: IXMLDocument;
+  pNode      : IXMLNode;
+  gNode      : IXMLNode;
+  cNode      : IXMLNode;
+begin
+  xmlDocument := TXMLDocument.Create(nil);
+  xmlDocument.LoadFromFile(strFileName);
+  xmlDocument.Active := True;
+  try
+    pNode := xmlDocument.DocumentElement;
+    if pNode <> nil then
+    begin
+      gNode := pNode.ChildNodes.FindNode('ItemDefinitionGroup');
+      if gNode = nil then
+        Exit;
+
+      cNode := gNode.ChildNodes.FindNode('ClCompile');
+      if cNode = nil then
+        cNode := gNode.AddChild('ClCompile');
+
+      cNode.AddChild('RuntimeLibrary').Text := 'MultiThreaded';
+      xmlDocument.SaveToFile(strFileName);
+    end;
+  finally
+    xmlDocument.Active := False;
+    xmlDocument        := nil;
+  end;
+end;
+
+procedure SearchAndReplaceInFile(const arrFile: TStringDynArray; const bCase: Boolean; const strSearch, strReplace, strEncoding: string; const bVCAddMT: Boolean);
 var
   sFile   : String;
   lstFile : TStringList;
@@ -67,6 +101,12 @@ begin
         Writeln(Format('%s 替换成功', [sFile]));
       end;
 
+      { 给 VC 工程文件添加 MT 编译 }
+      if bVCAddMT and (not bReplace) and SameText(ExtractFileExt(sFile), '.vcxproj') then
+      begin
+        VCAddMT(sFile);
+      end;
+
       lstFile.Clear;
     end;
   finally
@@ -79,7 +119,8 @@ begin
   Result := MidStr(strValue, 2, Length(strValue) - 2);
 end;
 
-procedure SearchAndReplace;
+{ 文件的字符串搜索替换 }
+procedure FilesSearchAndReplace;
 var
   strFilePath: String;
   strFileType: String;
@@ -92,7 +133,9 @@ var
   strParam   : string;
   arrParam   : TStringDynArray;
   I          : Integer;
+  bVCAddMT   : Boolean;
 begin
+  { 参数必须不少于 4 个 }
   strParam := string(GetCommandLine);
   I        := Pos(' ''', strParam);
   strParam := RightStr(strParam, Length(strParam) - I);
@@ -103,13 +146,25 @@ begin
     Exit;
   end;
 
-  bSubDir     := True;
-  bCase       := False;
   strFilePath := MiddleStr(arrParam[0]);
   strFileType := MiddleStr(arrParam[1]);
   strSearch   := MiddleStr(arrParam[2]);
   strReplace  := MiddleStr(arrParam[3]);
-  if Length(arrParam) = 7 then
+
+  { 其它 4 个参数 }
+  bSubDir     := True;
+  bCase       := False;
+  bVCAddMT    := False;
+  strEncoding := 'ASCII';
+
+  if Length(arrParam) = 8 then
+  begin
+    bSubDir     := StrToBool(arrParam[4]);
+    bCase       := StrToBool(arrParam[5]);
+    strEncoding := MiddleStr(arrParam[6]);
+    bVCAddMT    := StrToBool(arrParam[7]);
+  end
+  else if Length(arrParam) = 7 then
   begin
     bSubDir     := StrToBool(arrParam[4]);
     bCase       := StrToBool(arrParam[5]);
@@ -117,34 +172,34 @@ begin
   end
   else if Length(arrParam) = 6 then
   begin
-    bSubDir     := StrToBool(arrParam[4]);
-    bCase       := StrToBool(arrParam[5]);
-    strEncoding := 'ASCII';
+    bSubDir := StrToBool(arrParam[4]);
+    bCase   := StrToBool(arrParam[5]);
   end
   else if Length(arrParam) = 5 then
   begin
-    bSubDir     := StrToBool(arrParam[4]);
-    bCase       := False;
-    strEncoding := 'ASCII';
-  end
-  else if Length(arrParam) = 4 then
+    bSubDir := StrToBool(arrParam[4]);
+  end;
+
+  if bVCAddMT then
   begin
-    bSubDir     := True;
-    bCase       := False;
-    strEncoding := 'ASCII';
+    strEncoding := 'UTF8';
   end;
 
   if bSubDir then
     arrFiles := TDirectory.GetFiles(strFilePath, strFileType, TSearchOption.soAllDirectories)
   else
     arrFiles := TDirectory.GetFiles(strFilePath, strFileType, TSearchOption.soTopDirectoryOnly);
-  if Length(arrFiles) = 0 then
-    Exit;
 
-  SearchAndReplaceInFile(arrFiles, bCase, strSearch, strReplace, strEncoding);
+  if Length(arrFiles) = 0 then
+  begin
+    Writeln('没有找到满足条件的文件');
+    Exit;
+  end;
+
+  SearchAndReplaceInFile(arrFiles, bCase, strSearch, strReplace, strEncoding, bVCAddMT);
 end;
 
 begin
-  SearchAndReplace;
+  FilesSearchAndReplace;
 
 end.
